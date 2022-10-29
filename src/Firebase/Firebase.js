1bbb42df7f007d417e08ed1ac56504de8f5a4b4d
alpolok <abdullahalrahman3@gmail.com>
2022-10-29 22:35:03 +0300

    first commit

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCibPxjz9lsBK6cfBcOtMeVALVXhZNYDnQ",
+    authDomain: "recap59.firebaseapp.com",
+    projectId: "recap59",
+    storageBucket: "recap59.appspot.com",
+    messagingSenderId: "426668789586",
+    appId: "1:426668789586:web:94d0eda4d4b0804406748b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
